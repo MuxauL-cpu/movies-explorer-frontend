254edf9ab646227aed02a6875a7a542d73b5001b
Михаил <62656006+MuxauL-cpu@users.noreply.github.com>
2023-09-14 11:08:59 +0300

    Delete src/components directory

--- src/components/NavTab/NavTab.jsx
+++
@@ -1,9 +0,0 @@
-import './NavTab.css';
-
-function NavTab() {
-  return(
-    <a className='navtab' href='#about-project'>Узнать больше</a>
-  )
-}
-
-export default NavTab;
--- src/components/Title/Title.jsx
+++
@@ -1,9 +0,0 @@
-import './Title.css';
-
-function Title({ titleText }) {
-  return(
-    <h3 className='title'>{titleText}</h3>
-  )
-}
-
-export default Title;
